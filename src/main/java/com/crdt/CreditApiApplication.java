32b32b5685820b2a7abdeffa9464489fdcb12b64
yash <yashwanth.k@mooo.farm>
2023-07-28 19:42:19 +0530

    spring setup initialization

---
+++ src/main/java/com/crdt/CreditApiApplication.java
@@ -0,0 +1,13 @@
+package com.crdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crdt/CreditApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
